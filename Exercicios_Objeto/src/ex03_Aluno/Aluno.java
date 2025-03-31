1560aba04b211e5bc38bb7e2326ff6765f88d6bc
lucasdd0 <lucasfo2005@gmail.com>
2025-03-30 22:00:42 -0300

    commit

---
+++ Exercicios_Objeto/src/ex03_Aluno/Aluno.java
@@ -0,0 +1,54 @@
+package ex03_Aluno;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Aluno {
+    String nome;
+    int idade;
+    int rm;
+    ArrayList<Double> notas = new ArrayList<>();
+    double media;
+
+    void coletaDados(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite seu nome: ");
+        nome = sc.next();
+
+        System.out.println("Digite sua idade: ");
+        idade = sc.nextInt();
+
+        System.out.println("Digite seu rm: ");
+        rm = sc.nextInt();
+    }
+
+    void mostraIdentidade(){
+        System.out.println("Nome: " + nome + "\nIdade: " + idade + "\nRM: " + rm);
+    }
+
+    void coletaNotas(){
+        Scanner sc = new Scanner(System.in);
+
+        double nota;
+
+        while(true){
+            System.out.println("Digite a nota (ou -1 para finalizar): ");
+            nota = sc.nextDouble();
+
+            if(nota == -1){
+                break;
+            }
+
+            notas.add(nota);
+        }
+
+    }
+
+    void mostrarNotas(){
+        System.out.println("Notas do aluno " + nome);
+        for(Double nota :notas){
+            System.out.println(nota);
+        }
+    }
+
+}
---
+++ Exercicios_Objeto/src/ex03_Aluno/Main.java
@@ -0,0 +1,16 @@
+package ex03_Aluno;
+
+public class Main {
+    public static void main(String[] args) {
+        Aluno aluno1 = new Aluno();
+
+        aluno1.coletaDados();
+
+        aluno1.mostraIdentidade();
+
+        aluno1.coletaNotas();
+
+        aluno1.mostrarNotas();
+
+    }
+}
