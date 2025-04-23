1c4021475f5d47ffb6e04bd17a3b3a767467f5fe
lucasdd0 <lucasfo2005@gmail.com>
2025-04-23 17:50:39 -0300

    commit

--- Exercicios_Objeto/src/br/com/lucasdev/screenmatch/models/banco/ContaBancaria.java
+++ Exercicios_Objeto/src/br/com/lucasdev/screenmatch/models/banco/ContaBancaria.java
@@ -1,62 +1,24 @@
 package br.com.lucasdev.screenmatch.models.banco;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static java.lang.Double.parseDouble;
 import static javax.swing.JOptionPane.*;
 import static java.lang.Integer.parseInt;
-import static java.lang.Long.parseLong;
+import static java.lang.Double.parseDouble;
 
 public class ContaBancaria {
     private int numeroConta;
     private double saldo;
     private String titular;
-    private List<ContaBancaria> contas = new ArrayList<>();
-
-    public int getNumeroConta() {
-        return numeroConta;
-    }
-
-    public double getSaldo() {
-        return saldo;
-    }
-
-    public String getTitular() {
-        return titular;
-    }
-
-    public List<ContaBancaria> getContas() {
-        return contas;
-    }
-
-    public void setNumeroConta(int numeroConta) {
-        this.numeroConta = numeroConta;
-    }
-
-    public void setSaldo(double saldo) {
-        this.saldo = saldo;
-    }
-
-    public void setTitular(String titular) {
-        this.titular = titular;
-    }
-
-    public void setContas(List<ContaBancaria> contas) {
-        this.contas = contas;
-    }
 
     public void menuPrincipal() {
         int opcao = 0;
         String menu = "1. Cadastrar\n" +
                 "2. Depositar\n" +
                 "3. Sacar\n" +
-                "4. Menu Status\n" +
-                "5. Finalizar";
+                "4. Finalizar";
 
         do {
             opcao = parseInt(showInputDialog(menu));
-            if (opcao < 1 || opcao > 5) {
+            if (opcao < 1 || opcao > 4) {
                 showMessageDialog(null, "Opção inválida!");
             } else {
                 switch (opcao) {
@@ -70,35 +32,29 @@ public class ContaBancaria {
                         menuSacar();
                         break;
                     case 4:
-                        menuContas();
-                        break;
-                    case 5:
-                        showMessageDialog(null, "Programa finalizado!");
                         break;
                 }
             }
-        } while (opcao != 5);
+        } while (opcao != 4);
     }
 
     private void menuCadastro() {
         saldo = 0;
-        titular = showInputDialog("Digite o nome do titular: ");
-        numeroConta = parseInt(showInputDialog("Digite o número da conta: "));
-        contas.add(new ContaBancaria(titular, numeroConta));
+        titular = showInputDialog("Digite o seu nome: ");
+        numeroConta = parseInt(showInputDialog("Digite o número da sua conta: "));
+        saldo = parseDouble(showInputDialog("Digite seu saldo: "));
         showMessageDialog(null, "Conta cadastrada com sucesso!");
     }
 
     private void menuDepositar() {
-        double valor = parseDouble(showInputDialog("Quanto deseja depositar: "));
+        double valor = parseDouble(showInputDialog("Digite quanto deseja depositar: "));
         saldo += valor;
-        showMessageDialog(null, "Depósito realizado com sucesso! Novo saldo: R$" + String.format("%.2f", saldo));
+        showMessageDialog(null, "Seu saldo atual é R$" + saldo);
     }
 
     private void menuSacar() {
-        double valor = parseInt(showInputDialog("Quanto deseja sacar: "));
+        double valor = parseDouble(showInputDialog("Digite quanto deseja sacar: "));
         saldo -= valor;
-        showMessageDialog(null, "Saque realizado com sucesso! Novo saldo: R$" + String.format("%.2f", saldo));
+        showMessageDialog(null, "Seu saldo atual é R$" + saldo);
     }
-
-    public abstract void menuContas();
 }
--- Exercicios_Objeto/src/br/com/lucasdev/screenmatch/models/banco/Main.java
+++ Exercicios_Objeto/src/br/com/lucasdev/screenmatch/models/banco/Main.java
@@ -1,11 +1,11 @@
 package br.com.lucasdev.screenmatch.models.banco;
 
-import br.com.lucasdev.screenmatch.models.banco.ContaBancaria;
-
 public class Main {
     public static void main(String[] args) {
 
-        new ContaBancaria().menuPrincipal();
+        ContaBancaria conta = new ContaBancaria();
+
+        conta.menuPrincipal();
 
     }
 }
